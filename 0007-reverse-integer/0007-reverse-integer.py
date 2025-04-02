2d2b8794dd57f8f7fa7c9b839d546bd8568ccc60
devaharshavardhan <150235246+devaharshavardhan@users.noreply.github.com>
2025-04-02 22:52:47 +0530

    Time: 38 ms (65.73%), Space: 16.6 MB (100%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        l=(2**31)-1
+        o=-(2**31)
+        y=1*x
+        if x<0:
+            x=abs(x)
+        res=0
+        i=0
+        while(x>0):
+            z=x%10
+            res=res*10+z
+            i+=1
+            x=x//10
+        if y<0:
+            res=-res
+        if y>0 and res<=l:
+            return res
+        elif y<0  and res>=o:
+            return res
+        else:
+            return 0
+                
